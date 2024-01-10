232e16c8e9bd2410c117af4288c33174c95e2733
ugorjieze <zealousemmy12@gmail.com>
2024-01-10 16:01:55 +0100

    updated file

--- table.js
+++ table.js
@@ -1,67 +1,68 @@
-const globalUrl ="https://backend-learn-kmt8.onrender.com"
+const globalUrl = "https://backend-learn-kmt8.onrender.com";
 
 let usersRow = document.getElementById("users-roll");
-const table_name_body=document.getElementById("table-name-body");
+const table_name_body = document.getElementById("table-name-body");
 
 const tableContainer = document.getElementById("table-container");
 
-const tableDetailsContainer = document.getElementById("table-details-container")
+const tableDetailsContainer = document.getElementById(
+  "table-details-container"
+);
 
 const getPatientsName = async () => {
-    try{
-        console.log("started now")
-      const patients = await axios.get(`${globalUrl}/user/`)
+  try {
+    console.log("started now");
+    const patients = await axios.get(`${globalUrl}/user/`);
     //   console.log(patients, "from patient list")
-      const returnedPatients = await patients.data.msg
-      console.log(returnedPatients, "from patient list")
-      let table = `<table>`;
-      table +=`
+    const returnedPatients = await patients.data.msg;
+    console.log(returnedPatients, "from patient list");
+    let table = `<table>`;
+    table += `
         <thead>
             <tr class="row100 head">
                 <th class="cell100 column1">Patients Name</th>
             </tr>
         </thead>
-      `
+      `;
     //   let tr = `<tr class="row100 body">`
-      
-      for (let patient of returnedPatients){
-        table +=`
+
+    for (let patient of returnedPatients) {
+      table += `
         <tbody id="table-name-body">
             <tr class="row100 body" id="users-roll">
                 <td class="cell100 column1">${patient.patient_Name}</td>
             </tr>
         </tbody>
-        `
-        // tr += `<td class="cell100 column1">${patient.patient_Name}</td>`
+        `;
+      // tr += `<td class="cell100 column1">${patient.patient_Name}</td>`
 
-        console.log(patient, "patient")
-      }
-    //   tr += `</tr>`
-      table += `</table>`
-      tableContainer.innerHTML=table
-      console.log(table, "table")
-    //   table_name_body.innerHTML = tr
-      return tr;
-    }catch(err){
-      console.log(err)
+      console.log(patient, "patient");
     }
+    //   tr += `</tr>`
+    table += `</table>`;
+    tableContainer.innerHTML = table;
+    console.log(table, "table");
+    //   table_name_body.innerHTML = tr
+    return tr;
+  } catch (err) {
+    console.log(err);
   }
-  
+};
+
 //   getPatientsName();
 
 //   usersRow.innerHTML = getPatientsName()
 
-
 const getPatientsDetails = async () => {
-    try{
-        console.log("started now")
-      const patients = await axios.get(`${globalUrl}/user/`)
+  try {
+    console.log("started now");
+    const patients = await axios.get(`${globalUrl}/user/`);
     //   console.log(patients, "from patient list")
-      const returnedPatients = await patients.data.msg
-      console.log(returnedPatients, "from patient list")
+    const returnedPatients = await patients.data.msg;
+    console.log(returnedPatients, "from patient list");
 
-      let table = `<table>`;
-      table +=`
+    let table = `<table>`;
+    table += `
       <thead>
         <tr class="row100 head">
             <th class="cell100 column2">Patient Name</th>
@@ -81,51 +82,10 @@ const getPatientsDetails = async () => {
             <th class="cell100 column2">Other medical condition</th>
         </tr>
      </thead>
-      `
+      `;
     //   let tr = `<tr class="row100 body">`
-   
-MMJ_Card_exp_Date
-: 
-"2024-01-04"
-accept_privacy_policy_and_practice
-: 
-"false"
-address
-: 
-"address 123"
-cell_phone
-: 
-"0908765342"
-createdAt
-: 
-"2024-01-05T15:32:08.306Z"
-date_of_birth
-: 
-"2024-01-04"
-driver_License
-: 
-"driver 009"
-driver_License_exp_Date
-: 
-"2024-02-11"
-email
-: 
-"queen@lmnurse.ca"
-home_phone
-: 
-"0908765467"
-patient_Name
-: 
-"Julie"
-primary_care_physician
-: 
-"lsdkldll"
-registered_caregiver
-: 
-"some care giver"
-      
-      for (let patient of returnedPatients){
-        table +=`
+    for (let patient of returnedPatients) {
+      table += `
         <tbody>
             <tr class="row100 body">
                 <td class="cell100 column2">${patient?.patient_Name}</td>
@@ -145,20 +105,20 @@ registered_caregiver
                 <td class="cell100 column2">424242xxxxxx6262</td>
             </tr>
         </tbody>
-        `
-        // tr += `<td class="cell100 column1">${patient.patient_Name}</td>`
+        `;
+      // tr += `<td class="cell100 column1">${patient.patient_Name}</td>`
 
-        console.log(patient, "patient")
-      }
-    //   tr += `</tr>`
-      table += `</table>`
-      tableDetailsContainer.innerHTML=table
-      console.log(table, "table")
-    //   table_name_body.innerHTML = tr
-      return tr;
-    }catch(err){
-      console.log(err)
+      console.log(patient, "patient");
     }
+    //   tr += `</tr>`
+    table += `</table>`;
+    tableDetailsContainer.innerHTML = table;
+    console.log(table, "table");
+    //   table_name_body.innerHTML = tr
+    return tr;
+  } catch (err) {
+    console.log(err);
   }
+};
 
-  getPatientsDetails()
+getPatientsDetails();
